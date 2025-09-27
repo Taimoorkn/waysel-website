eace1e01013d84c4ee6255fcb7bafbaf44091672
TaimoorKhan071 <taimoorkn2221@gmail.com>
2025-09-27 19:21:40 +0500

    d

--- src/app/home/content/StarWars.js
+++ src/app/home/content/StarWars.js
@@ -9,7 +9,7 @@ const StarWars = () => {
     const generateBinaryText = () => {
       const screenWidth = window.innerWidth;
       // Estimate characters that fit based on screen width and letter spacing
-      const charactersNeeded = Math.floor(screenWidth / 20.5); // Adjust 20 based on font size and spacing
+      const charactersNeeded = Math.floor(screenWidth / 30); // Adjust 20 based on font size and spacing
       let text = "";
       for (let i = 0; i < charactersNeeded; i++) {
         text += Math.random() > 0.5 ? "1" : "0";
@@ -28,7 +28,7 @@ const StarWars = () => {
       {/* Top Binary Pattern */}
       <div className="absolute left-0 top-0 h-1/2 w-full overflow-hidden">
         <div
-          className="relative h-full w-full font-mono text-gray-300/80"
+          className="relative h-full w-full font-mono text-gray-300/50"
           style={{
             transform: "perspective(170px) rotateX(-25deg)",
             transformOrigin: "center top",
@@ -37,16 +37,17 @@ const StarWars = () => {
           {Array.from({ length: 120 }, (_, i) => (
             <motion.div
               key={i}
-              className="absolute w-full text-center opacity-65"
+              className="absolute w-full text-center"
               animate={{
                 y: [-100, 700],
-                letterSpacing: ["0.8rem", "0.02rem"],
+                letterSpacing: ["0.8rem", "0.2rem"],
+                fontSize: ["2rem", "1rem"],
               }}
               transition={{
-                duration: 12,
+                duration: 30,
                 repeat: Infinity,
                 ease: "linear",
-                delay: i * 0.6,
+                delay: i * 1.5,
               }}
             >
               {binaryText}
@@ -69,19 +70,20 @@ const StarWars = () => {
             transformOrigin: "center bottom",
           }}
         >
-          {Array.from({ length: 120 }, (_, i) => (
+          {Array.from({ length: 110 }, (_, i) => (
             <motion.div
               key={i}
               className="absolute w-full text-center opacity-65"
               animate={{
                 y: [700, -100],
-                letterSpacing: ["0.8rem", "0.02rem"],
+                letterSpacing: ["0.8rem", "0.2rem"],
+                fontSize: ["2rem", "1rem"],
               }}
               transition={{
-                duration: 12,
+                duration: 30,
                 repeat: Infinity,
                 ease: "linear",
-                delay: (112 - i) * 0.6,
+                delay: (103 - i) * 1,
               }}
             >
               {binaryText}
