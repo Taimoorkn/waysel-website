79f9d8e8426b49b4a734fa1602414c5f193790f6
TaimoorKhan071 <taimoorkn2221@gmail.com>
2025-10-06 18:35:17 +0500

    3

--- src/app/home/content/StarWars.js
+++ src/app/home/content/StarWars.js
@@ -5,15 +5,18 @@ import AnimatedSVG from "@/components/AnimatedSVG";
 
 const StarWars = () => {
   return (
-    <div className="relative h-[60vh] w-full overflow-hidden bg-black text-white xl:h-[90vh]">
+    <div className="relative h-[60vh] w-full overflow-hidden bg-primary_bg text-white xl:h-[90vh]">
       {/* Top Binary Pattern */}
-      <div className="absolute left-0 top-0 z-50 w-full overflow-hidden">
+      <div className="absolute left-0 top-0 w-full overflow-hidden">
         <AnimatedSVG />
       </div>
 
       {/* Main Content */}
-      <div className="absolute inset-0 z-10 mt-[10%] flex items-center justify-center xl:mt-[2%]">
+      <div className="absolute inset-0 mx-auto mt-[10%] flex max-w-5xl items-center justify-center border xl:mt-[2%]">
         <SectionHeading title="We are a team of innovators dedicated to solving complex challenges with elegant digital solutions." />
+      </div>
+      <div className="absolute bottom-0 left-0 w-full scale-y-[-1] overflow-hidden">
+        <AnimatedSVG />
       </div>
     </div>
   );
