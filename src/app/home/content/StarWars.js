2963a54405f3469e85c19b8f286f97fcf1968455
TaimoorKhan071 <taimoorkn2221@gmail.com>
2025-10-06 18:41:24 +0500

    33

--- src/app/home/content/StarWars.js
+++ src/app/home/content/StarWars.js
@@ -1,21 +1,22 @@
 import React from "react";
-
 import SectionHeading from "../../../components/SectionHeading";
 import AnimatedSVG from "@/components/AnimatedSVG";
 
 const StarWars = () => {
   return (
-    <div className="relative h-[60vh] w-full overflow-hidden bg-primary_bg text-white xl:h-[90vh]">
+    <div className="relative mt-10 flex w-full flex-col overflow-hidden bg-primary_bg text-white">
       {/* Top Binary Pattern */}
-      <div className="absolute left-0 top-0 w-full overflow-hidden">
+      <div className="w-full overflow-hidden">
         <AnimatedSVG />
       </div>
 
       {/* Main Content */}
-      <div className="absolute inset-0 mx-auto mt-[10%] flex max-w-5xl items-center justify-center border xl:mt-[2%]">
+      <div className="mx-auto flex max-w-5xl flex-1 items-center justify-center py-12">
         <SectionHeading title="We are a team of innovators dedicated to solving complex challenges with elegant digital solutions." />
       </div>
-      <div className="absolute bottom-0 left-0 w-full scale-y-[-1] overflow-hidden">
+
+      {/* Bottom Binary Pattern (flipped) */}
+      <div className="w-full scale-y-[-1] overflow-hidden">
         <AnimatedSVG />
       </div>
     </div>
